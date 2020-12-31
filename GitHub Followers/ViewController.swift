c49d690da62435ef3be335e419f44f64b14809ce
Dedi Prakasa <dedprakasa@gmail.com>
2020-12-31 22:27:04 +0700

    init new project

---
+++ GitHub Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub Followers
+//
+//  Created by Dedi Prakasa on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
